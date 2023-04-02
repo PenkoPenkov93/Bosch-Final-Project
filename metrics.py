bbad51408e3a356567c2662ffb3dd66fbf266b01
PenkoPenkov93 <110025708+PenkoPenkov93@users.noreply.github.com>
2023-04-02 14:47:40 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,10 +1,17 @@
-from prometheus_client import Counter, Gauge, Info, start_http_server
+from prometheus_client import Counter, Gauge, Info, start_http_server, REGISTRY
 import psutil
+import time
+
+# Unregister the existing 'github_runner_status' metric, if it exists
+if 'github_runner_status' in REGISTRY._names_to_collectors:
+    REGISTRY.unregister(REGISTRY._names_to_collectors['github_runner_status'])
 
 # Create Prometheus metrics
 workflow_duration = Gauge('github_workflow_run_duration_ms', 'Duration of the GitHub Actions workflow in milliseconds')
 workflow_status = Gauge('github_workflow_run_status', 'Status of the GitHub Actions workflow (0 for success, 1 for failure)')
-runner_status = Gauge('github_runner_status', 'Operating system of the GitHub Actions runner')
+
+# Create the new 'runner_status' Info metric
+runner_status = Info('runner_status', 'Operating system of the GitHub Actions runner')
 
 # Start the Prometheus metrics server
 start_http_server(8000)
@@ -20,10 +27,7 @@ disk_usage = psutil.disk_usage('/').total / 1024 / 1024 / 1024
 # Update the Prometheus metrics
 workflow_duration.set(100)  # Replace with the actual duration of the workflow
 workflow_status.set(0)  # Replace with the actual status of the workflow (0 for success, 1 for failure)
-runner_status = Info('github_runner_status', 'Operating system of the GitHub Actions runner')
 runner_status.info({'os': 'Linux'})
-
 
 # Wait for the Prometheus metrics server to receive the metrics
 time.sleep(1)
-
