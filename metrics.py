3044c3f021be701fc1eb9bc476c82404aca56c74
PenkoPenkov93 <110025708+PenkoPenkov93@users.noreply.github.com>
2023-04-02 14:39:10 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,7 +1,13 @@
+from prometheus_client import Counter, Gauge, start_http_server
 import psutil
-import requests
-import json
-import os
+
+# Create Prometheus metrics
+workflow_duration = Gauge('github_workflow_run_duration_ms', 'Duration of the GitHub Actions workflow in milliseconds')
+workflow_status = Gauge('github_workflow_run_status', 'Status of the GitHub Actions workflow (0 for success, 1 for failure)')
+runner_status = Gauge('github_runner_status', 'Operating system of the GitHub Actions runner')
+
+# Start the Prometheus metrics server
+start_http_server(8000)
 
 # Get process time and memory usage
 process = psutil.Process()
@@ -11,21 +17,11 @@ memory_usage = process.memory_info().rss / 1024 / 1024
 # Get disk usage
 disk_usage = psutil.disk_usage('/').total / 1024 / 1024 / 1024
 
-# Get GitHub Actions environment variables
-workflow_run_duration = os.environ.get('GITHUB_WORKFLOW_RUN_DURATION')
-workflow_run_status = os.environ.get('GITHUB_WORKFLOW_RUN_STATUS')
-runner_status = os.environ.get('RUNNER_OS')
+# Update the Prometheus metrics
+workflow_duration.set(100)  # Replace with the actual duration of the workflow
+workflow_status.set(0)  # Replace with the actual status of the workflow (0 for success, 1 for failure)
+runner_status.set('Linux')  # Replace with the actual operating system of the runner
 
-# Define Prometheus metrics payload
-metrics = {
-    'process_time': process_time,
-    'memory_usage': memory_usage,
-    'disk_usage': disk_usage,
-    'github_workflow_run_duration_ms': workflow_run_duration,
-    'github_workflow_run_status': workflow_run_status,
-    'github_runner_status': runner_status
-}
+# Wait for the Prometheus metrics server to receive the metrics
+time.sleep(1)
 
-# Send metrics to Prometheus
-url = 'http://104.214.223.114:9100/metrics/job/metrics_exporter'
-response = requests.post(url, data=json.dumps(metrics))
