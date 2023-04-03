f6623e6c504826724ff62d2dcd2d56d56b93394d
root <root@ubuntu2004.linuxvmimages.local>
2023-04-03 20:25:51 +0300

    My-app changes

--- myapp.py
+++ myapp.py
@@ -1,25 +1,82 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List, Optional
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+DATABASE_URL = "sqlite:///./penkodb.db"
 
 app = FastAPI()
 
-class Item(BaseModel):
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    price = Column(Float)
+    tax = Column(Float)
+
+Base.metadata.create_all(bind=engine)
+
+class ItemCreate(BaseModel):
     name: str
-    description: str = None
+    description: Optional[str] = None
     price: float
-    tax: float = None
+    tax: Optional[float] = None
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/", response_model=List[Item])
+async def read_root(db: Session = Depends(get_db)):
+    items = db.query(Item).all()
+    return items
 
-@app.post("/items/")
-async def create_item(item: Item):
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
     return item
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    new_item = Item(**item.dict())
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: int, item: ItemCreate, db: Session = Depends(get_db)):
+    db_item = db.query(Item).filter(Item.id == item_id).first()
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    for key, value in item.dict().items():
+        setattr(db_item, key, value)
+    db.commit()
+    return db_item
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int, db: Session = Depends(get_db)):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    db.delete(item)
+    db.commit()
+    return {"result": "Item deleted"}
 
 @app.post("/login/")
 async def login(username: str, password: str):
